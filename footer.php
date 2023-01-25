93d2b024d946b4a5961964bf04ccd37da1833ab3
clairoo <121592705+Clairooooo@users.noreply.github.com>
2023-01-26 07:33:39 +0800

    tampilan admin

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    <strong>backend &copy; <?= date('Y'); ?>. FA Team</strong> BA211 <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+    
+</footer>
